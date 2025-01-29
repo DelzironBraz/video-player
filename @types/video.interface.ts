6b2e2e0d772d232febd40346efe0197ed5becf40
Delziron Lima <delziron.braz@hotmail.com>
2025-01-29 02:37:07 -0300

    chore(update video interface)

--- @types/video.interface.ts
+++ @types/video.interface.ts
@@ -1,8 +1,10 @@
 export interface IVideoPlayer {
   id?: string;
   thumbnail: string;
+  category: string;
   video: string;
   title: string;
   subTitle: string;
   description: string;
+  status: string;
 }
