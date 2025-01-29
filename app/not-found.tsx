b52d2236f31351dc502d4e725624bb023c242d3a
Delziron Lima <delziron.braz@hotmail.com>
2025-01-29 16:57:54 -0300

    style(not found page)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,14 @@
+import { MainContainer, MaxContainer } from "@/templates/container.template";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <MainContainer>
+      <MaxContainer className="h-screen">
+        <h2 className="text-2xl font-semibold">Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link href="/">Return Home</Link>
+      </MaxContainer>
+    </MainContainer>
   );
 }
